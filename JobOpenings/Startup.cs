6333b5eca86b3b5cd1fdea7e21b1ad3a343d38e8
Diana Khlomko <kim.keiraff@gmail.com>
2021-04-27 09:29:14 +0300

    Connecting to a db

--- JobOpenings/Startup.cs
+++ JobOpenings/Startup.cs
@@ -2,12 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using JobOpenings.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
 
 namespace JobOpenings
 {
@@ -23,6 +25,9 @@ namespace JobOpenings
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            string connection = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<JobOpeningsContext>(options =>
+            options.UseSqlServer(connection));
             services.AddControllersWithViews();
         }
 
