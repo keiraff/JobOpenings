50363a2e5a799061ef36534e99c124fc9bed35e1
Ольга Чувак <greyvilka@gmail.com>
2021-05-01 12:30:24 +0300

    Add BD connection

---
+++ JobOpenings/dbo.Submits.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Submits] (
+    [Id]          INT            IDENTITY (1, 1) NOT NULL,
+    [Name]        NVARCHAR (MAX) NULL,
+    [Surname]     NVARCHAR (MAX) NULL,
+    [MobilePhone] NVARCHAR (MAX) NULL,
+    [Email]       NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_Submits] PRIMARY KEY CLUSTERED ([Id] ASC), 
+    CONSTRAINT [FK_Submits_Vacancies] FOREIGN KEY ([Id]) REFERENCES [dbo].[Vacancies]([Id])
+);
+
